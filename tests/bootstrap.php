bd545c6a1d8977780410971d9fb108906f85bd82
Luu Hoang Nam <hoangnam0705@icloud.com>
2014-09-01 07:23:36 +0700

    First commit

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+$loader->addPsr4( 'Speedify\\', __DIR__ . '/Speedify' );
